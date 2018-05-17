788fcaad93721e114f71e01e04952b65acbaa180
Maxence Delaine <maxence.delaine@gmail.com>
2018-05-17 23:58:06 +0200

    Adding a lot of stuff about database tables, routes...

--- back/filldb.js
+++ back/filldb.js
@@ -1,8 +1,8 @@
-let sqlite = require("sqlite")
-const characters = require("./characters")
-let db = sqlite.open("./database.sqlite", { Promise })
-for (let i = 0; i < characters.length; i++) {
-  console.log(i)
-  let sql = `INSERT INTO users (id, name, gender, image, species) VALUES ("${characters[i].id}", "${characters[i].name}", "${characters[i].gender}", "${characters[i].image}", "${characters[i].species}");`
-  db.then(_db => _db.run(sql))
-}
+let sqlite = require('sqlite')
+const characters = require('./characters')
+let db = sqlite.open('./database.sqlite', { Promise })
+// for (let i = 0; i < characters.length; i++) {
+//   console.log(i)
+//   let sql = `INSERT INTO users (id, name, gender, image, species) VALUES ("${characters[i].id}", "${characters[i].name}", "${characters[i].gender}", "${characters[i].image}", "${characters[i].species}");`
+//   db.then(_db => _db.run(sql))
+// }
--- back/server.js
+++ back/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const cors = require('cors')
+const fs = require('fs')
 const sqlite = require('sqlite')
 
 const db = require('./db')
 const characters = require('./characters')
 const filldb = require('./filldb')
+const fileEncoding = 'utf8'
 
 const app = express()
 const port = process.env.PORT || 2222
@@ -15,6 +18,7 @@ app.use(express.static('public'))
 app.use(methodOverride('__method'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 const html = `
 <!doctype html>
@@ -39,10 +43,32 @@ const html = `
   </body>
 </html>
 `
+// Read all the database and return it
+const getCharactersFromDb = () => {
+  const allCharactersSQLStatement = 'SELECT * FROM users'
+  return db.then(_db => _db.all(allCharactersSQLStatement))
+}
 
+// Insert data about profile in table ACCOUNT
+const insertProfileInDb = (pseudo, password) => {
+  const insertProfileSQLStatement = `INSERT INTO account (pseudo, password) VALUES (${req.body.pseudo}, ${req.body.password})`
+  return db.then(_db => _db.run(insertProfileSQLStatement))
+}
+
+// New GET / route
 app.get('/', (req, res) => {
   res.send(html)
   res.end()
+})
+
+// New GET /characters route
+app.get('/characters', (req, res) => {
+  getCharactersFromDb().then(function (value) { res.send(value).end() })
+})
+
+// New POST /account route
+app.post('/account', (req, res) => {
+  insertProfileInDb().then(function (value) { res.send(value).end() })
 })
 
 module.exports = app
